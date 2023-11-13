923944ad831ae1d1baa595b745f367989c4c73f9
Nabil <nabilkajeiou2000@gmail.com>
2023-11-13 18:28:42 +0100

    feat(HomeActivity): position GPS added

--- app/src/main/java/paci/main/activities/logged/HomeActivity.java
+++ app/src/main/java/paci/main/activities/logged/HomeActivity.java
@@ -1,8 +1,19 @@
 package paci.main.activities.logged;
+import android.Manifest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +31,10 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.TravelMode;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import paci.main.LoginActivity;
 import paci.main.R;
 
@@ -28,6 +43,7 @@ public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
     private TextView utilisateurTextView;
     private EditText editTextStart, editTextDestination;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +64,18 @@ public class HomeActivity extends AppCompatActivity {
                         utilisateurTextView.setText("Bienvenue " + username);
                     }
                     editTextStart = findViewById(R.id.editTextStart);
+                    Button buttonMyLocation = findViewById(R.id.buttonMyLocation);
+                    buttonMyLocation.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            checkLocationPermission();
+                        }
+                    });
+
+
                     editTextDestination = findViewById(R.id.editTextDestination);
+
+
                     Button submitButton = findViewById(R.id.buttonSubmit);
                     submitButton.setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -69,6 +96,97 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    private void checkLocationPermission() {
+        Toast.makeText(this, "Bouton cliqué", Toast.LENGTH_SHORT).show();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Demande de permission de localisation", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            Toast.makeText(this, "Permission de localisation déjà accordée", Toast.LENGTH_SHORT).show();
+            getUserLocation();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission de localisation accordée", Toast.LENGTH_SHORT).show();
+                getUserLocation();
+            } else {
+                Toast.makeText(this, "Permission de localisation refusée", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void getUserLocation() {
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (locationManager != null) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        // Une fois que la localisation change, utilisez la nouvelle localisation
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        String userLocation = latitude + ", " + longitude;
+
+                        String address = getAddressFromCoordinates(userLocation);
+
+                        EditText editTextStart = findViewById(R.id.editTextStart);
+                        editTextStart.setText(address);
+
+                        Toast.makeText(getApplicationContext(), "Nouvelle adresse : " + address, Toast.LENGTH_SHORT).show();
+
+                        locationManager.removeUpdates(this);
+                    }
+
+                });
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            Toast.makeText(this, "Le gestionnaire de localisation n'est pas disponible", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    private String getAddressFromCoordinates(String coordinates) {
+        String[] latLng = coordinates.split(", ");
+        double latitude = Double.parseDouble(latLng[0]);
+        double longitude = Double.parseDouble(latLng[1]);
+
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+
+                StringBuilder addressStringBuilder = new StringBuilder();
+                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                    addressStringBuilder.append(address.getAddressLine(i)).append("\n");
+                }
+
+                return addressStringBuilder.toString().trim();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Adresse introuvable";
+    }
+
 
     private void displayErrorAndNavigateToMain() {
         Toast.makeText(HomeActivity.this, "Une erreur est survenue.", Toast.LENGTH_SHORT).show();
@@ -80,7 +198,6 @@ public class HomeActivity extends AppCompatActivity {
         String startPoint = editTextStart.getText().toString();
         String destination = editTextDestination.getText().toString();
 
-        // Utilisez l'API Directions de Google Maps pour obtenir des informations sur le trajet.
         GeoApiContext geoApiContext = new GeoApiContext.Builder()
                 .apiKey(getString(R.string.google_client_id))
                 .build();
@@ -98,6 +215,9 @@ public class HomeActivity extends AppCompatActivity {
             // Convertissez la durée en heures.
             double durationInHours = durationInSeconds / 3600.0;
 
+            // Convertir la durée en heures en minutes sans changer la fonction calculateTotalCost
+            double durationInMinutes = durationInHours * 60;
+
             // Calcul du coût total du trajet (prix du trajet + rémunération du chauffeur).
             double totalCost = calculateTotalCost(durationInHours);
 
@@ -109,11 +229,11 @@ public class HomeActivity extends AppCompatActivity {
             double estimatedFuelUsage = distanceInKm * fuelConsumptionRate;
 
             // Affichez les résultats avec une justification détaillée.
-            String justification = String.format("Durée estimée : %.2f heures\n" +
+            String justification = String.format("Durée estimée : %.2f minutes\n" +
                             "Distance estimée : %.2f km\n" +
                             "Quantité estimée de carburant : %.2f litres\n" +
                             "Coût du trajet : %.2f euros (incluant 30 euros de rémunération pour le chauffeur)",
-                    durationInHours, distanceInKm, estimatedFuelUsage, totalCost);
+                    durationInMinutes, distanceInKm, estimatedFuelUsage, totalCost);
 
             Toast.makeText(this, justification, Toast.LENGTH_LONG).show();
 
