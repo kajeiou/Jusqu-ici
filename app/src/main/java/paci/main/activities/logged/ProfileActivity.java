ddd7cb2e2eedddb715149ccd16ef6b13fb092164
Nabil <nabilkajeiou2000@gmail.com>
2023-11-14 21:44:43 +0100

    feat(ProfileActivity): picture profil can be changed

--- app/src/main/java/paci/main/activities/logged/ProfileActivity.java
+++ app/src/main/java/paci/main/activities/logged/ProfileActivity.java
@@ -1,24 +1,33 @@
 package paci.main.activities.logged;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.Manifest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import paci.main.LoginActivity;
 import paci.main.R;
-
 public class ProfileActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     private ImageView profileImageView;
     private TextView usernameTextView;
@@ -32,7 +41,6 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        // Initialiser les vues
         profileImageView = findViewById(R.id.profileImageView);
         usernameTextView = findViewById(R.id.username);
         emailTextView = findViewById(R.id.email);
@@ -40,7 +48,7 @@ public class ProfileActivity extends AppCompatActivity {
         logoutButton = findViewById(R.id.logoutButton);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        // Charger les informations de l'utilisateur depuis Firebase
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // Nom d'utilisateur
@@ -48,44 +56,44 @@ public class ProfileActivity extends AppCompatActivity {
             if (username != null && !username.isEmpty()) {
                 usernameTextView.setText(username);
             } else {
-                // Si le nom d'utilisateur n'est pas défini, afficher l'adresse e-mail
+
                 usernameTextView.setText(user.getEmail());
             }
 
-            // Adresse e-mail
+
             String email = user.getEmail();
             if (email != null && !email.isEmpty()) {
                 emailTextView.setText(email);
             }
 
-            // TODO: Charger la photo de profil à partir de Firebase Storage ou autre source
-            // Utilisez Picasso, Glide, ou une autre bibliothèque pour le chargement d'image asynchrone
         }
 
-        // Gérer le clic sur le bouton de changement de photo
         changePhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: Implémenter la logique pour changer la photo de profil
-                // Vous pouvez ouvrir la galerie ou la caméra et mettre à jour l'ImageView
+                if (ContextCompat.checkSelfPermission(ProfileActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    openGallery();
+                } else {
+
+                    ActivityCompat.requestPermissions(ProfileActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PICK_IMAGE_REQUEST);
+                }
             }
         });
 
-        // Gérer le clic sur le bouton de déconnexion
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Se déconnecter de Firebase Authentication
                 FirebaseAuth.getInstance().signOut();
 
-                // Rediriger vers l'écran de connexion par exemple
                 Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
                 startActivity(intent);
-                finish(); // Fermer l'activité actuelle
+                finish();
             }
         });
 
-        // Gérer la navigation en bas
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -93,16 +101,48 @@ public class ProfileActivity extends AppCompatActivity {
                     case R.id.menu_item1:
                         Intent intentHome = new Intent(ProfileActivity.this, HomeActivity.class);
                         startActivity(intentHome);
-                        finish(); // Fermer l'activité actuelle
+                        finish();
                         return true;
                     case R.id.menu_item2:
-                        // Fragment ou activité pour le menu_item2
+
                         return true;
-                    // Ajoutez des cas pour les autres éléments du menu
+
                     default:
                         return false;
                 }
             }
         });
+    }
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PICK_IMAGE_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // La permission a été accordée, ouvrir la galerie
+                openGallery();
+            } else {
+                Toast.makeText(this, "Permission refusée, impossible de sélectionner une image", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+
+            Glide.with(this)
+                    .load(selectedImageUri)
+                    .apply(new RequestOptions().circleCrop())
+                    .into(profileImageView);
+        }
     }
 }
